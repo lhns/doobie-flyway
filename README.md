77a4b099dabd4e20ba6bc17bc5973b9224d69746
Pierre Kisters <pierrekisters@gmail.com>
2022-07-04 13:45:03 +0200

    refactoring

--- README.md
+++ README.md
@@ -5,9 +5,11 @@
 [![Apache License 2.0](https://img.shields.io/github/license/lhns/doobie-flyway.svg?maxAge=3600)](https://www.apache.org/licenses/LICENSE-2.0)
 [![Scala Steward badge](https://img.shields.io/badge/Scala_Steward-helping-blue.svg?style=flat&logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAQCAMAAAARSr4IAAAAVFBMVEUAAACHjojlOy5NWlrKzcYRKjGFjIbp293YycuLa3pYY2LSqql4f3pCUFTgSjNodYRmcXUsPD/NTTbjRS+2jomhgnzNc223cGvZS0HaSD0XLjbaSjElhIr+AAAAAXRSTlMAQObYZgAAAHlJREFUCNdNyosOwyAIhWHAQS1Vt7a77/3fcxxdmv0xwmckutAR1nkm4ggbyEcg/wWmlGLDAA3oL50xi6fk5ffZ3E2E3QfZDCcCN2YtbEWZt+Drc6u6rlqv7Uk0LdKqqr5rk2UCRXOk0vmQKGfc94nOJyQjouF9H/wCc9gECEYfONoAAAAASUVORK5CYII=)](https://scala-steward.org)
 
+Flyway migrations for doobie.
+
 ### build.sbt
 ```sbt
-libraryDependencies += "de.lolhens" %% "doobie-flyway" % "0.1.0"
+libraryDependencies += "de.lolhens" %% "doobie-flyway" % "0.2.0"
 ```
 
 ## License
--- core/src/main/scala/de/lhns/doobie/flyway/Flyway.scala
+++ core/src/main/scala/de/lhns/doobie/flyway/Flyway.scala
@@ -1,94 +1,91 @@
 package de.lhns.doobie.flyway
 
+import cats.Functor
 import cats.effect.{Resource, Sync}
-import cats.syntax.flatMap._
+import cats.syntax.functor._
 import doobie.Transactor
 import org.flywaydb.core.api.MigrationInfoService
 import org.flywaydb.core.api.configuration.FluentConfiguration
 import org.flywaydb.core.api.output._
 
 import javax.sql.DataSource
-import scala.util.chaining._
+
+final class Flyway[F[_]] private(configuration: FluentConfiguration) {
+  def configure(f: FluentConfiguration => FluentConfiguration): Flyway[F] =
+    new Flyway(f(new FluentConfiguration().configuration(configuration)))
+
+  def configureF(f: FluentConfiguration => F[FluentConfiguration])(implicit F: Functor[F]): F[Flyway[F]] =
+    f(new FluentConfiguration().configuration(configuration)).map(new Flyway(_))
+
+  def apply[B](f: FluentConfiguration => F[B]): F[B] =
+    f(configuration)
+
+  def info()(implicit F: Sync[F]): F[MigrationInfoService] = apply { configuration =>
+    F.blocking {
+      configuration
+        .load()
+        .info()
+    }
+  }
+
+  def migrate()(implicit F: Sync[F]): F[MigrateResult] = apply { configuration =>
+    Sync[F].blocking {
+      configuration
+        .load()
+        .migrate()
+    }
+  }
+
+  def validate()(implicit F: Sync[F]): F[Either[ValidateResult, ValidateResult]] = apply { configuration =>
+    Sync[F].blocking {
+      val result = configuration
+        .load()
+        .validateWithResult()
+
+      Either.cond(
+        result.validationSuccessful || configuration.isCleanOnValidationError,
+        result,
+        result
+      )
+    }
+  }
+
+  def baseline()(implicit F: Sync[F]): F[BaselineResult] = apply { configuration =>
+    Sync[F].blocking {
+      configuration
+        .load()
+        .baseline()
+    }
+  }
+
+  def repair()(implicit F: Sync[F]): F[RepairResult] = apply { configuration =>
+    Sync[F].blocking {
+      configuration
+        .load()
+        .repair()
+    }
+  }
+
+  def clean(confirm: Boolean)(implicit F: Sync[F]): F[CleanResult] = apply { configuration =>
+    Sync[F].blocking {
+      require(confirm, "please confirm that you want to clean the database")
+      configuration
+        .load()
+        .clean()
+    }
+  }
+}
 
 object Flyway {
   def apply[F[_], A <: DataSource, B](transactor: Transactor.Aux[F, A])
-                                     (f: FluentConfiguration => F[B]): Resource[F, B] =
-    Resource.eval(transactor.configure { dataSource =>
-      org.flywaydb.core.Flyway.configure
-        .dataSource(dataSource)
-        .pipe(f)
-    })
+                                     (f: Flyway[F] => F[B]): Resource[F, B] = Resource.eval {
+    transactor.configure { dataSource =>
+      val flyway = new Flyway[F](
+        org.flywaydb.core.Flyway.configure
+          .dataSource(dataSource)
+      )
 
-  def info[F[_] : Sync, A <: DataSource](transactor: Transactor.Aux[F, A])
-                                        (configure: FluentConfiguration => F[FluentConfiguration]): Resource[F, MigrationInfoService] =
-    Flyway(transactor) {
-      configure(_)
-        .flatMap(configuration => Sync[F].blocking {
-          configuration
-            .load()
-            .info()
-        })
+      f(flyway)
     }
-
-  def migrate[F[_] : Sync, A <: DataSource](transactor: Transactor.Aux[F, A])
-                                           (configure: FluentConfiguration => F[FluentConfiguration]): Resource[F, MigrateResult] =
-    Flyway(transactor) {
-      configure(_)
-        .flatMap(configuration => Sync[F].blocking {
-          configuration
-            .load()
-            .migrate()
-        })
-    }
-
-  def validate[F[_] : Sync, A <: DataSource](transactor: Transactor.Aux[F, A])
-                                            (configure: FluentConfiguration => F[FluentConfiguration]): Resource[F, Either[ValidateResult, ValidateResult]] =
-    Flyway(transactor) {
-      configure(_)
-        .flatMap(configuration => Sync[F].blocking {
-          val result = configuration
-            .load()
-            .validateWithResult()
-
-          Either.cond(
-            result.validationSuccessful || configuration.isCleanOnValidationError,
-            result,
-            result
-          )
-        })
-    }
-
-  def baseline[F[_] : Sync, A <: DataSource](transactor: Transactor.Aux[F, A])
-                                            (configure: FluentConfiguration => F[FluentConfiguration]): Resource[F, BaselineResult] =
-    Flyway(transactor) {
-      configure(_)
-        .flatMap(configuration => Sync[F].blocking {
-          configuration
-            .load()
-            .baseline()
-        })
-    }
-
-  def repair[F[_] : Sync, A <: DataSource](transactor: Transactor.Aux[F, A])
-                                          (configure: FluentConfiguration => F[FluentConfiguration]): Resource[F, RepairResult] =
-    Flyway(transactor) {
-      configure(_)
-        .flatMap(configuration => Sync[F].blocking {
-          configuration
-            .load()
-            .repair()
-        })
-    }
-
-  def clean[F[_] : Sync, A <: DataSource](transactor: Transactor.Aux[F, A], confirm: Boolean = false)
-                                         (configure: FluentConfiguration => F[FluentConfiguration]): Resource[F, CleanResult] =
-    Flyway(transactor) {
-      configure(_)
-        .flatMap(configuration => Sync[F].blocking {
-          require(confirm, "please confirm that you want to clean the database")
-          configuration
-            .load()
-            .clean()
-        })
-    }
+  }
 }
