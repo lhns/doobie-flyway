50dad66761a9438f3acc31c9e307be3cee74eacd
Pierre Kisters <pierrekisters@gmail.com>
2023-01-12 10:28:12 +0100

    resource transformation helpers

--- core/src/main/scala/de/lhns/doobie/flyway/package.scala
+++ core/src/main/scala/de/lhns/doobie/flyway/package.scala
@@ -6,7 +6,9 @@ import org.flywaydb.core.api.resource.LoadableResource
 import org.flywaydb.core.api.{MigrationInfoService, ResourceProvider}
 import org.flywaydb.core.internal.scanner.{LocationScannerCache, ResourceNameCache, Scanner}
 
+import java.io.{Reader, StringReader}
 import java.util
+import java.util.stream.Collectors
 
 package object flyway {
   implicit class FluentConfigurationOps(val configuration: FluentConfiguration) extends AnyVal {
@@ -24,6 +26,46 @@ package object flyway {
         new LocationScannerCache,
         configuration.isFailOnMissingLocations
       )
+    }
+
+    def mapResourceProvider(f: ResourceProvider => ResourceProvider): FluentConfiguration =
+      configuration.resourceProvider(f(resourceProviderOrDefault))
+  }
+
+  private def readString(reader: Reader): String = {
+    val charArray = new Array[Char](8 * 1024)
+    val builder = new java.lang.StringBuilder()
+    var numCharsRead: Int = 0
+    while ( {
+      numCharsRead = reader.read(charArray, 0, charArray.length)
+      numCharsRead != -1
+    }) {
+      builder.append(charArray, 0, numCharsRead)
+    }
+    builder.toString
+  }
+
+  implicit class LoadableResourceOps(val resource: LoadableResource) extends AnyVal {
+    def mapContent(f: String => String): LoadableResource = new LoadableResource {
+      override def read(): Reader = new StringReader(f(readString(resource.read())))
+
+      override def getAbsolutePath: String = resource.getAbsolutePath
+
+      override def getAbsolutePathOnDisk: String = resource.getAbsolutePathOnDisk
+
+      override def getFilename: String = resource.getFilename
+
+      override def getRelativePath: String = resource.getRelativePath
+    }
+  }
+
+  implicit class ResourceProviderOps(val resourceProvider: ResourceProvider) extends AnyVal {
+    def mapResource(f: LoadableResource => LoadableResource): ResourceProvider = new ResourceProvider {
+      override def getResource(name: String): LoadableResource =
+        Option(resourceProvider.getResource(name)).map(f).orNull
+
+      override def getResources(prefix: String, suffixes: Array[String]): util.Collection[LoadableResource] =
+        resourceProvider.getResources(prefix, suffixes).stream().map(f(_)).collect(Collectors.toList())
     }
   }
 }
